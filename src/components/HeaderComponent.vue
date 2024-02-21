ac7fc29bf8adeb828b54cc9c128fdd7b7dd5af8a
ddarahakit2023 <ddarahakit2023@gmail.com>
2024-02-21 14:22:10 +0900

    jenkins test3

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,7 +14,7 @@
                 </a>
                 <ul class="topMenu">
                     <li v-show="!isLoggedIn">
-                        <a href="/signup/member">Sign Up</a>
+                        <a href="/signup/member">가입</a>
                     </li>
                     <li v-show="!isLoggedIn">
                         <a href="/login/member">Login</a>
